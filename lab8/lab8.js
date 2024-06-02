887dcb1121aa2af43eb611b55dee37d9a34676d7
Влад Печенкин <mogucci2000@list.ru>
2024-06-02 18:40:24 +0700

    День недели

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,3 +1,18 @@
+function showDate() {
+    let out = document.getElementById('current-date');
+    let today = new Date();
+    out.innerHTML = `
+        <div class="frame">
+            <div>Дата и время для локали России: ${today.toLocaleString('ru-RU')}</div>
+            <div>Дата и время для локали Египта: ${today.toLocaleString('ar-EG')}</div>
+            <div>Дата и время для локали Швейцарии: ${today.toLocaleString('fr-CH')}</div>
+            <div>Дата и время для локали Албании: ${today.toLocaleString('sq-AL')}</div>
+            <div>Дата и время для локали Германии: ${today.toLocaleString('de-DE')}</div>
+            <div>Дата и время для локали Канады: ${today.toLocaleString('en-CA')}</div>
+        </div>
+    `;
+}
+
 function getDayOfWeek() {
     const day = parseInt(document.getElementById('day').value);
     const month = parseInt(document.getElementById('month').value);
