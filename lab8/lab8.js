62bf33d43d258ee976634c681947fee53dd177b2
Влад Печенкин <mogucci2000@list.ru>
2024-06-02 18:05:10 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,14 +1,10 @@
 function showDate() {
-    let out = document.getElementById('current-date');
-    let today = new Date();
-    out.innerHTML = `
-        <div class="frame">
-            <div>Дата и время для локали России: ${today.toLocaleString('ru-RU')}</div>
-            <div>Дата и время для локали Египта: ${today.toLocaleString('ar-EG')}</div>
-            <div>Дата и время для локали Швейцарии: ${today.toLocaleString('fr-CH')}</div>
-            <div>Дата и время для локали Албании: ${today.toLocaleString('sq-AL')}</div>
-            <div>Дата и время для локали Германии: ${today.toLocaleString('de-DE')}</div>
-            <div>Дата и время для локали Канады: ${today.toLocaleString('en-CA')}</div>
-        </div>
-    `;
+    var now = new Date();
+    var days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+    var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+    document.getElementById('year').textContent = 'Текущий год: ' + now.getFullYear();
+    document.getElementById('month').textContent = 'Текущий месяц: ' + months[now.getMonth()];
+    document.getElementById('date').textContent = 'Текущая дата: ' + now.getDate();
+    document.getElementById('day').textContent = 'День недели: ' + days[now.getDay()];
 }
